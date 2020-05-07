ae51879a09b6651da16aa813bf90d66352ebd92a
allenz-0 <allenz@rpi.edu>
2020-05-06 20:37:25 -0400

    Update application.py

--- application.py
+++ application.py
@@ -7,8 +7,10 @@ def main():
     This should not be run until retrieve.py, database.py, and load_data.py (in that order)
     :return:null
     """
+    
     app = Database()
     print("Application Loaded\nWelcome User")
+
     # ---Begin User Input--- #
     user_query_selection = -1
     while user_query_selection != 0:
@@ -18,17 +20,20 @@ def main():
                                      "\n4. Query 4"
                                      "\n5. Query 5"
                                      "\nPlease select an option, or enter 0 to exit:")
+
         # ---Error Checking for input--- #
         # Handle non integer input
         if type(user_query_selection) != int:
             user_query_selection = -1
             print("Error: Please enter a valid choice")
             continue
+
         # Handle an out of bounds input
         if user_query_selection > 5 or user_query_selection < 0:
             user_query_selection = -1
             print("Error: Please enter an integer within the range of selection [0-5]")
             continue
+
         # ---Translate Input Into Running The Query--- #
         if user_query_selection == 1:
             print("Run Query 1")
