c7dfa238fe3b2e5da8eee2ec07a3a7af07042751
Joey Cengel <t80088@gmail.com>
2020-05-06 23:44:01 -0400

    Added import_crashes

--- load_data2.py
+++ load_data2.py
@@ -54,21 +54,45 @@ def import_weather():
     connection.commit()
 
 
+def null_number(s):
+    if s == "":
+        return 1
+    return 0
+
 def import_crashes():
     cur = connection.cursor()
     with open('user_accounts.csv', 'r') as crash_file:
         file_reader = csv.reader(crash_file)
         next(file_reader)  # Skip the header row.
         for row in file_reader:
-            # crash
-            # location
-            # injuries
-            # Injuries
-            # Deaths
-            # VehicleFactors
+            # Check for missing latitude and longitude
+            if null_number(row[4]):
+                row[4] = 0
+
+            if null_number(row[5]):
+                row[5] = 0
+
             cur.execute(
-                "INSERT INTO users VALUES (%s, %s, %s, %s)",
-                row
+                "INSERT INTO Crash VALUES(%s, %s, %s)", (row[23], row[0], row[1])
+            )
+
+            cur.execute(
+                "INSERT INTO Location VALUES(%s, %s, %s, %s, %s, %s, %s, %s)", 
+                (row[23], row[2], row[3], row[4], row[5], row[7], row[8], row[9])
+            )
+
+            cur.execute(
+                "INSERT INTO Injuries VALUES(%s, %s, %s, %s, %s)", (row[23], row[10], row[12], row[14], row[16])
+            )
+
+            cur.execute(
+                "INSERT INTO Deaths VALUES(%s, %s, %s, %s, %s)", (row[23], row[11], row[13], row[15], row[17])
+            )
+
+            cur.execute(
+                "INSERT INTO VehiclesFactors VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", 
+                (row[23], row[24], row[25], row[26], row[27], row[28], 
+                row[18], row[19], row[20], row[21], row[22])
             )
     connection.commit()
 
