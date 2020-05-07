7ed1c11ffb065e41b7b492d9f0379721d7b5990b
allenz-0 <allenz@rpi.edu>
2020-05-06 21:11:05 -0400

    Add "help" option

--- application.py
+++ application.py
@@ -1,6 +1,17 @@
 from database import Database
 
 
+printQueryOptions(options):
+    print()
+
+    current = 1
+    for option in options:
+        print("{}. {}".format(current, option[0]))
+        current += 1
+
+    print()
+
+
 def main():
     """
     Run the actual application and deal with user input.
@@ -10,11 +21,7 @@ def main():
 
     app = Database()
     print("Application Loaded\nWelcome User")
-
-    # NEW CODE
-    # ------------------------------------------------------------------------------------
-    # ------------------------------------------------------------------------------------
-    # ------------------------------------------------------------------------------------
+    
 
     query_options = [
         ("Crashes by date", app.crashesByDate),
@@ -24,17 +31,20 @@ def main():
         ("Deadliest weather", app.deadliestWeather)
     ]
 
-    print("\nEnter the number of the query you would like to execute, or type quit:")
-    current = 1
-    for option in query_options:
-        print("{}. {}".format(current, option[0]))
-        current += 1
+    print("\nEnter the number of the query you would like to execute:")
 
+    printQueryOptions(query_options)
+
+    print("\nType quit to stop, or help to list the options again.\n")
 
     user_query_selection = ""
 
     while (user_query_selection != "quit"):
         user_query_selection = input("\nSelection: ")
+
+        if (user_query_selection == "help"):
+            printQueryOptions(query_options)
+            continue
 
         if (user_query_selection == "quit"):
             continue
@@ -46,45 +56,6 @@ def main():
         query_options[user_query_selection][1]()
 
 
-    # ------------------------------------------------------------------------------------
-    # ------------------------------------------------------------------------------------
-    # ------------------------------------------------------------------------------------
-
-
-    # ---Begin User Input--- #
-    user_query_selection = -1
-    while user_query_selection != 0:
-        user_query_selection = input("\n1. Query 1"
-                                     "\n2. Query 2"
-                                     "\n3. Query 3"
-                                     "\n4. Query 4"
-                                     "\n5. Query 5"
-                                     "\nPlease select an option, or enter 0 to exit:")
-
-        # ---Error Checking for input--- #
-        # Handle non integer input
-        if type(user_query_selection) != int:
-            user_query_selection = -1
-            print("Error: Please enter a valid choice")
-            continue
-
-        # Handle an out of bounds input
-        if user_query_selection > 5 or user_query_selection < 0:
-            user_query_selection = -1
-            print("Error: Please enter an integer within the range of selection [0-5]")
-            continue
-
-        # ---Translate Input Into Running The Query--- #
-        if user_query_selection == 1:
-            print("Run Query 1")
-        elif user_query_selection == 2:
-            print("Run Query 2")
-        elif user_query_selection == 3:
-            print("Run Query 3")
-        elif user_query_selection == 4:
-            print("Run Query 4")
-        elif user_query_selection == 5:
-            print("Run Query 5")
 
 
 if __name__ == "__main__":
