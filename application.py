366b898ba9de2e62a2bc54d737a592d60eb6578f
allenz-0 <allenz@rpi.edu>
2020-05-06 21:24:02 -0400

    Comments

--- application.py
+++ application.py
@@ -1,35 +1,40 @@
 from database import Database
 
 
+app = Database()    # Object to interface with the database
+
+# List of query options and their corresponding function objects
+query_options = [
+    ("Crashes by date", app.crashesByDate),
+    ("Weather by date", app.weatherByDate),
+    ("Most common weather", app.mostCommonWeather),
+    ("Most crashed in weather", app.crashesByWeather),
+    ("Deadliest weather", app.deadliestWeather)
+]
+
+
 printQueryOptions(options):
+    """
+    Prints out numbered list of query options
+    options is a list of tuples of the form ("option name", function object)
+    """
+
     print()
 
     current = 1
     for option in options:
         print("{}. {}".format(current, option[0]))
         current += 1
-
     print()
+
 
 
 def main():
     """
-    Run the actual application and deal with user input.
+    Define 
     This should not be run until retrieve.py, database.py, and load_data.py (in that order)
     :return:null
     """
-
-    app = Database()
-    print("Application Loaded\nWelcome User")
-    
-
-    query_options = [
-        ("Crashes by date", app.crashesByDate),
-        ("Weather by date", app.weatherByDate),
-        ("Most common weather", app.mostCommonWeather),
-        ("Most crashed in weather", app.crashesByWeather),
-        ("Deadliest weather", app.deadliestWeather)
-    ]
 
     print("\nEnter the number of the query you would like to execute:")
 
@@ -52,10 +57,9 @@ def main():
         if (type(user_query_selection) != int or user_query_selection < 1 or user_query_selection > len(query_options)-1):
             print("Invalid selection.")
             continue
-
+        
+        # Execute selected query function
         query_options[user_query_selection][1]()
-
-
 
 
 if __name__ == "__main__":
