90a73a98c95343ff6ea12500c2eda943103057c9
allenz-0 <allenz@rpi.edu>
2020-05-06 21:00:15 -0400

    Proposed change to application.py

--- application.py
+++ application.py
@@ -7,9 +7,44 @@ def main():
     This should not be run until retrieve.py, database.py, and load_data.py (in that order)
     :return:null
     """
-    
+
     app = Database()
     print("Application Loaded\nWelcome User")
+
+    # NEW CODE
+    # ------------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------------------
+
+    query_options = [
+        ("Crashes by date", app.crashesByDate),
+        ("Weather by date", app.weatherByDate),
+        ("Most common weather", app.mostCommonWeather),
+        ("Most crashed in weather", app.crashesByWeather),
+        ("Deadliest weather", app.deadliestWeather)
+    ]
+
+    print("\nEnter the number of the query you would like to execute, or type quit:")
+    current = 1
+    for option in query_options:
+        print("{}. {}".format(current, option[0]))
+        current += 1
+
+
+    while (True):
+        user_query_selection = input("\nSelection: ")
+
+        if (type(user_query_selection) != int or user_query_selection < 1 or user_query_selection > len(query_options)-1):
+            print("Invalid selection.")
+            continue
+
+        query_options[user_query_selection][1]()
+
+
+    # ------------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------------------
+
 
     # ---Begin User Input--- #
     user_query_selection = -1
